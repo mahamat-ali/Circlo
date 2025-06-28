07698a345c75481cfb8bd77f1ac3d6a312b03719
mahamat-ali <terapfils30@gmail.com>
2025-06-28 20:16:17 +0100

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import "../../global.css";
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
+
+const RootLayout = () => {
+  return <Slot />
+}
+
+export default RootLayout;
